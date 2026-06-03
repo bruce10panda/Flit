04d1efb7ed2e7285a4adc7662bf6bb60c0a81608
pjanda <131262420+bruce10panda@users.noreply.github.com>
2026-06-03 18:58:36 +0200

    Initial commit

---
+++ src-tauri/gen/android/app/src/main/java/com/pjanda/flit/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pjanda.flit
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
